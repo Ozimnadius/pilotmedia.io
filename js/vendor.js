08bf9d6d7f7222cb7261fb610d03d8ae8b60a59c
Mikle Seregin <mikhail-seregin@yandex.ru>
2021-03-31 11:55:14 +0300

    add block cost

--- js/vendor.js
+++ js/vendor.js
@@ -4,3 +4,4 @@
 // @prepros-append ../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
 // @prepros-append ../node_modules/swiper/swiper-bundle.js
 // @prepros-append ../node_modules/parallax-js/dist/parallax.js
+// @prepros-append ../node_modules/jquery-form-styler/dist/jquery.formstyler.js
